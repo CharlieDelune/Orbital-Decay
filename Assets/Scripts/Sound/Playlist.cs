3e5296bfe55ac1b783790af8469be90b7aa561d5
jevlin <42493630+jevlin@users.noreply.github.com>
2020-05-04 14:59:38 +0200

    Adding assets (#37)
    * Adding assets
    
    * Update QualitySettings.asset
    
    * Jeff Ninja'd In Credits, Music, and Fonts

---
+++ Assets/Scripts/Sound/Playlist.cs
@@ -0,0 +1,35 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class Playlist : MonoBehaviour
+ {
+    [SerializeField] private AudioClip[] soundtrack;
+    [SerializeField] private AudioSource audioSource;
+
+    [SerializeField] private int currentTrack;
+
+    void Start ()
+    {
+        currentTrack = 0;
+        if (!audioSource.playOnAwake)
+        {
+            audioSource.clip = soundtrack[currentTrack];
+            audioSource.Play();
+        }
+    }
+
+    void Update ()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = soundtrack[currentTrack];
+            audioSource.Play();
+            currentTrack++;
+
+            if (currentTrack >= soundtrack.Length)
+            {
+                currentTrack = 0;
+            }
+        }
+    }
+ }
